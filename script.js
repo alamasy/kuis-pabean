7aba2aebe9f030e42a964b02973bbbb44d8dbe31
devaaa <devaaamee@gmail.com>
2023-09-10 11:30:36 +0700

    First commit

---
+++ script.js
@@ -0,0 +1,199 @@
+const content = [
+  {
+    id: "1",
+    pertanyaan: "Kawasan pabean adalah...",
+    pilihanJawaban: [
+      "Wilayah RI yang meliputi darat, air, dan ruang udara di atasnya termasuk tempat-tempat tertentu di Zona Ekonomi Ekslusif dan Landasan Kontinen dimana berlaku undang-undang kepabeanan",
+      "WIlayah RI dimana berlaku undang-undang kepabeanan",
+      "Kawasan dengan batas-batas tertentu di daerah Pabean Indonesia lainnya",
+      "Kawasan dengan batas-batas tertentu di pelabuhan laut, bandar udara, atau tempat lain yang ditetapkan untuk lalu lintas barang yang sepenuhnya di bawah pengawasan DJBC",
+    ],
+    jawaban: "3",
+    tentang: "Terminologi Dalam Kepabeanan",
+  },
+  {
+    id: "2",
+    pertanyaan:
+      "Barang yang telah dimuat di sarana pengangkut dengan tujuan untuk diekspor, jika sarana pengangkutnya masih sandar di pelabuhan...",
+    pilihanJawaban: [
+      "Belum diberlakukan sebagai barang ekspor",
+      "Diperlakukan sebagai barang antar pulau",
+      "Diperlakukan sebagai barang yang berasal dari daerah bebas",
+      "Diperlakukan sebagai barang ekspor",
+    ],
+    jawaban: "3",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "3",
+    pertanyaan: "Tempat penimbunan sementara adalah tempat untuk menimbun...",
+    pilihanJawaban: [
+      "Barang impor, barang ekspor, dan barang antar pulau yang sementara menunggu pemuatan pengeluarannya",
+      "Barang impor, barang ekspor, barang yang tidak dikuasai, dan barang dikuasai negara sementara menunggu pemuatan atau pengeluarannya",
+      "Barang impor, barang ekspor, dan barang yag dinyatakan dikuasai negara sementara menunggu pemuatan atau pengeluarannya",
+      "Barang impor dan barang ekspor yang sementara menunggu pengeluaran dan pemuatanannya",
+    ],
+    jawaban: "3",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "4",
+    pertanyaan:
+      "Tarif Bea Masuk dapat ditetapkan berbeda dengan pasal 13 ayat 1 (setinggi-tingginya 40%) dengan alasan...",
+    pilihanJawaban: [
+      "Barang impor yang bersangkutan dikenakan tarif Bea Masuk berdasarkan perjanjian atau kesepakatan international",
+      "Perlindungan industri dalam negeri",
+      "Nilai pabean dicurigai diberitahukan dengan tidak benar",
+      "Untuk meningkatkan penerimaan negara dari sektor impor",
+    ],
+    jawaban: "0",
+    tentang: "Pengenaan Tarif Preferensi",
+    bagian: "",
+  },
+  {
+    id: "5",
+    pertanyaan: "Barang diangkut lanjut adalah...",
+    pilihanJawaban: [
+      "Barang dari luar daerah pabean untuk tujuan luar daerah pabean lainnya yang pengangkutannya singgah di salah satu pelabuhan di Indonesia",
+      "Barang yang diangkut dengan sarana pengengkut melalui kantor pabeam melalui pembongkaran terlebih dahulu",
+      "Barang yang belum dilunasi Bea Masuknya yang dipindahkapalkan atau diangkut terus ke luar daerah pabean",
+      "Barang yang diangkut dengan sarana pengangkut melalui kantor pebean tanpa melalui pembongkaran telebih dahulu",
+    ],
+    jawaban: "1",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "6",
+    pertanyaan: "Alasan pengenaan bea masuk anti dumping adalah...",
+    pilihanJawaban: [
+      "Harga ekspor barang yang lebih rendah dari harga nominalnya",
+      "Adanya subsidi terhadap barang ekspor",
+      "Pemasukan pajak sebesar-besarnya ke kas negara",
+      "Kebijakan pemerintah",
+    ],
+    jawaban: "0",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "7",
+    pertanyaan:
+      "Ketentuan tentang penunjukan Tempat Penimbunan Sementara diatur oleh...",
+    pilihanJawaban: [
+      "Pengusaha tempat penimbunan sementara",
+      "Administrator pelabuahn/bandara",
+      "Menteri Perhubungan",
+      "Menteri Keuangan",
+    ],
+    jawaban: "3",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "8",
+    pertanyaan: "Di tempat penimbunan pabean, disimpan...",
+    pilihanJawaban: [
+      "Barang impor dan barang ekspor sementara menunggu pengeluaran atau pemuatannya",
+      "Barang impor untuk tujuan produksi, pameran, poenjualan dan penimbunan",
+      "Barang import di bawah pengawasan pabean",
+      "Barang yang dinyatakan tidak dikuasai, barang dikuasai negara, dan barang yang menjadi milik negara",
+    ],
+    jawaban: "3",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "9",
+    pertanyaan: "Rumus menghitung bea masuk adalah...",
+    pilihanJawaban: [
+      "Persentase BTBMI x harga FOB",
+      "Persentase BTBMI x harga pasar di negara pengekspor",
+      "Persentase BTBMI x nilai pabean",
+      "Persentase BTBMI x NDPBM",
+    ],
+    jawaban: "2",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "10",
+    pertanyaan:
+      "Dewasa ini jenis tarif bea masuk yang berlaku untuk impor gula dan beras adalah...",
+    pilihanJawaban: [
+      "Tarif degresif",
+      "Tarif advalorum",
+      "Tarif spesifik",
+      "Tarif progresif",
+    ],
+    jawaban: "2",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "11",
+    pertanyaan: "Caran menghitung PPnBM...",
+    pilihanJawaban: [
+      "Persentase tarif x CIF",
+      "Persentase tarif x FOB",
+      "Persentase tarif (CIF + pungutan pabean)",
+      "10% x (CIF + pungutan pabean)",
+    ],
+    jawaban: "2",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+];
+
+const tentang = document.getElementById("tentang");
+const pertanyaan = document.getElementById("pertanyaan");
+const btnRender = document.getElementById("btn-render");
+const jawaban = document.querySelectorAll("#pilihan");
+// const jawaban2 = document.querySelectorAll(".jawaban");
+
+// const urut = 1
+const jumlahSoal = content.length;
+let urut = 0;
+
+// NILAI
+let nilai = 0;
+let times = 1;
+
+// render tampilan soal
+function renderUI() {
+  urut = Math.floor(Math.random() * (jumlahSoal - 0) + 0);
+
+  tentang.textContent = content[urut].tentang;
+  pertanyaan.textContent = content[urut].pertanyaan;
+
+  jawaban.forEach((el, i) => {
+    el.textContent = content[urut].pilihanJawaban[i];
+  });
+}
+
+renderUI();
+
+btnRender.addEventListener("click", () => {
+  renderUI();
+
+  jawaban.forEach((el) => {
+    el.classList.remove("green-text");
+    el.classList.remove("red-text");
+  });
+});
+
+jawaban.forEach((el, i) => {
+  el.addEventListener("click", () => {
+    times++;
+
+    const userAnswer = el.dataset.jawaban;
+
+    if (userAnswer === content[urut].jawaban) {
+      el.classList.add("green-text");
+    } else {
+      el.classList.add("red-text");
+    }
+  });
+});
