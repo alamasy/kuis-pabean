d4ad3573fae83be2c6f19d530230627f8d6b9618
devaaa <devaaamee@gmail.com>
2023-09-11 21:13:07 +0700

    30 soal

--- script.js
+++ script.js
@@ -145,6 +145,261 @@ const content = [
     tentang: "Tata Laksana Ekspor Impor",
     bagian: "",
   },
+  {
+    id: "12",
+    pertanyaan: "Pengurusan pemberitahuan pabean...",
+    pilihanJawaban: [
+      "Wajib dilakukan oleh importir atau eksportir",
+      "Wajib dilakukan oleh PPJK",
+      "Dapat dilakukan oleh siapa saja",
+      "Dilakukan oleh PPJK bilamana importir atau eksportir tidak melakukannya sendiri",
+    ],
+    jawaban: "3",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "13",
+    pertanyaan: "PPJK bertanggung jawab atas bea masuk...",
+    pilihanJawaban: [
+      "Setiap barang impor yang diurusnya",
+      "Jika diperjanjikan di dalam surat kuasa dari importir kepada PPJK",
+      "Jika importir tidak ditemukan",
+      "Jika diminta oleh pejabat pabean",
+    ],
+    jawaban: "2",
+    tentang: "Kewajiban dan tanggung jawab bea masuk",
+    bagian: "",
+  },
+  {
+    id: "14",
+    pertanyaan: "Hak menagih atas utang bea masuk akan kadaluwarsa setelah...",
+    pilihanJawaban: [
+      "30 hari sejak terbitnya kewajiban membayar",
+      "60 hari sejak terbitnya kewajiban membayar",
+      "10 tahun sejak terbitnya kewajiban membayar",
+      "24 bulan sejak terbitnya kewajiban membayar",
+    ],
+    jawaban: "2",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "15",
+    pertanyaan: "Salah satu alasan barang dinyatakan tidak dikuasai adalah...",
+    pilihanJawaban: [
+      "Barang yang disimpan di tempat penimbunan sementara lebih dari 30 hari",
+      "Barang berada di bawah pengawasan pabean",
+      "Barang tersangkut tindak pidana yang pelakunya tidak dikenal",
+      "Barang kena lartas yang tidak diberitahukan dalam pemberitahuan pabean atau barang diberitahukan secara tidak benar",
+    ],
+    jawaban: "0",
+    tentang: "Pembongkaran dan Penimbunan",
+    bagian: "",
+  },
+  {
+    id: "16",
+    pertanyaan:
+      "Yang menjadi alasan sebuah barang menjadi milik negara adalah...",
+    pilihanJawaban: [
+      "Barang yang disimpan di tempat penimbunan sementara melehbihi waktu 30 hari",
+      "Barang yang berada di bawah pengawasan pabean",
+      "Barang yang tersangkut tindak pidana yang pelakunya tidak diketahui",
+      "Barang yang dibatasi yang tidak diselesaikan pemiliknya dalam jangka waktu yang ditetapkan",
+    ],
+    jawaban: "3",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "17",
+    pertanyaan:
+      "Barang yang dilarang atau dibatasi ekspornya, yang diberitahukan dalam pemberitahuan ekspor...",
+    pilihanJawaban: [
+      "Atas permintaan eksportir dapat dibatalkan ekspornya",
+      "Dicegah oleh pejabat pabean",
+      "Dianggap sebagai barang yang dikuasai negara",
+      "Dianggap sebagai barang yang tidak dikuasai",
+    ],
+    jawaban: "",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "18",
+    pertanyaan:
+      "Didalam pasif action procedure, pejabat pabean dapat menangguhkan sementara waktu pengerluaran barang impor atau barang ekspor hasil pelanggaran hak kekayaan intelektual...",
+    pilihanJawaban: [
+      "Berdasarkan perintah tertulis ketua pengadilan niaga",
+      "Karena jabatan",
+      "Berdasarkan perintah dirjen bea cukai",
+      "Berdasarkan perintah kepala kantor pabean",
+    ],
+    jawaban: "0",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "19",
+    pertanyaan:
+      "Barang kiriman pos yang ditolak si alamat dan tidak dapat dikirim kembali...",
+    pilihanJawaban: [
+      "Dinyatakan sebagai barang dikuasai negara",
+      "Barang milik negara",
+      "Barang yang tidak dikuasai",
+      "Barang tahanan",
+    ],
+    jawaban: "2",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "20",
+    pertanyaan: "Daerah pabean indonesia adalah wilayah RI yang meliputi...",
+    pilihanJawaban: [
+      "Wilayah darat, perairan, dan ruang udara di atasnya termasuk zona ekslusif dan landasan kontinen",
+      "Wilayah darat, laut, dan ruang udara di atasnya, laut lepas dan laut wilayah",
+      "Wilayah darat, laut, dan ruang udara serta tempat tertentu di wilayah RI",
+      "Wilayah darat, perairan, dan ruang udara di atasnya, termasuk tempat-tempat tertentu di zona ekonomi ekslusif dan landasan kontinen",
+    ],
+    jawaban: "3",
+    tentang: "Terminologi Pabean",
+    bagian: "",
+  },
+  {
+    id: "21",
+    pertanyaan: "Pemeriksaan pabean adalah...",
+    pilihanJawaban: [
+      "Pemeriksaan fisik barang",
+      "Meliputi pemeriksaan fisik dan penelitian dokumen",
+      "Pemeriksaan dokumen",
+      "Pemeriksaan terhadap penumpang atau awak sarana pengangkut",
+    ],
+    jawaban: "1",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "22",
+    pertanyaan:
+      "Berdasarkan pasal 112 undang-undang No.10 tahun 1995, penyidikan tindak pidana di bidang kepabeanan dilakukan oleh...",
+    pilihanJawaban: [
+      "Penyidik POLRI",
+      "Penyidik bea cukai dan penyidik polri",
+      "Kejaksaan",
+      "Penyidik bea cukai",
+    ],
+    jawaban: "3",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "23",
+    pertanyaan:
+      "Sesuai UU No.10 tahun 1995, dalah hal pelaku tindak pidana adalah suatu badan hukum atau perusahaan, maka sanksi pidana adalah suatu badan hukum atau perusahaan, maka sanksi pidana berupa pidana pokok yang dijatuhkan...",
+    pilihanJawaban: [
+      "Dapat berupa hukuman badan dan denda",
+      "Dapat berupa hukuman dan sanksi",
+      "Berupa pidana denda",
+      "Tergantung keputusan hakim",
+    ],
+    jawaban: "2",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "24",
+    pertanyaan:
+      "Barang impor atau ekspor yang berasal dari tindak pidana penyelundupan yang dirampas untuk negara, pemanfaaatannya diatur oleh...",
+    pilihanJawaban: [
+      "Hakim",
+      "Jaksa Penuntut Umum",
+      "Dirjen bea dan cukai",
+      "Menteri Keuangan",
+    ],
+    jawaban: "3",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "25",
+    pertanyaan:
+      "Pembukuan, catatan, dan surat menyurat yang bertalian dengan impor dan ekspor wajib disimpan oleh importir dan eksportir...",
+    pilihanJawaban: ["10 tahun", "2 tahun", "5 tahun", "8 tahun"],
+    jawaban: "0",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "26",
+    pertanyaan:
+      "PPJK yang melakukan tindak pidana kepabeaanan atas kuasa yang diterimanya dari importir...",
+    pilihanJawaban: [
+      "Tidak dapat dikenakan sanksi pidana karena hanya perpanjangan tangan dari importir",
+      "Dapat dikenakan sanksi administrasi",
+      "Tidak dapat dikenakan sanksi administrasi atau sanksi pidana karena ia bertindak atas perintah importir",
+      "Dikenakan sanksi pidana dengan ancaman pidana yang sama yang juga berlaku terhadap importir",
+    ],
+    jawaban: "3",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "27",
+    pertanyaan:
+      "Tindak pidana di bidang kepabeanan tidak dapat dituntut setelah jangka waktu...",
+    pilihanJawaban: [
+      "10 tahun sejak diserahkan pemberitahuan pabean atau sejak terjadinya tindak pidana",
+      "2 tahun sejak diserahkan pemberitahuan pabean atau sejak terjadinya tindak pidana",
+      "5 tahun sejak diserahkan pemberitahuan pabean atau sejak terjadinya tindak pidana",
+      "Tidak ada ketentuan yang mengatur di dalam UU pabean",
+    ],
+    jawaban: "0",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "28",
+    pertanyaan:
+      "Tanggung jawab atas keamanan segel yang dipasang pejabat, berada pada...",
+    pilihanJawaban: [
+      "Pemilik dan/atau yang menguasai sarana pengangkut atau tempat yang disegel",
+      "Pegawai DJBC yang memasang segel",
+      "Pemilik dan/atau yang menguasai sarana pengangkut atau tempat yang disegel atau pegawai DJBC yang memasang segel",
+      "Pemilik barang",
+    ],
+    jawaban: "0",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "29",
+    pertanyaan:
+      "Dalam hal sarana pengangkut atau tempat lain terdapat barang di bawah pengawasan pabean dan hal tersebut perlu diawasi oleh pejabat bea dan cukai, maka...",
+    pilihanJawaban: [
+      "Pengangkut atau pengusaha yang bersangkutan wajib memberikan bantuan yang layak dalam hal di tempat tersebtu tidak tersedia akomodasi",
+      "Akomodasi bagi pegawai DJBC menjadi tanggung jawab pemerintah",
+      "Pengangkut atau pengusaha tidak wajihb memberikan akomodasi kaerna tidak ada dasar peraturannya",
+      "Permintaan akomodasi bagi pejabat BC adalah merupakan pungutan liar",
+    ],
+    jawaban: "0",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
+  {
+    id: "30",
+    pertanyaan:
+      "Besarnya sanksi administrasi karena pelanggaran kepabeanan, dalah hal tarif  bea masuk adalah 0%, adalah...",
+    pilihanJawaban: [
+      "Nihil",
+      "100% dari bea masuk dan pajak dalam rangka impor",
+      "100% - 500% dari kekurangan pembayaran bea masuk",
+      "Rp 5.000.000",
+    ],
+    jawaban: "3",
+    tentang: "Tata Laksana Ekspor Impor",
+    bagian: "",
+  },
 ];
 
 const tentang = document.getElementById("tentang");
